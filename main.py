7a3962808d8ce2c2311069d4e8492a89a6aa127a
itshowrohitworks <therohitshrivastava@gmail.com>
2026-03-16 03:00:57 +0530

    Added the Block, Non-blocking and Normal endpoints.

--- main.py
+++ main.py
@@ -8,4 +8,35 @@ app = FastAPI()
 def home():
     return {"message":"Hello, World"}
 
+
+"""
+Blocking Endpoint: Runs synchronously
+"""
+@app.get("/blocking")
+async def blocking_io():
+    print("Hello")
+    time.sleep(5)
+    print("Bye")
+    return {"message":"Operation Completed!"}
+
+"""
+Non-Blocking Async Endpoint: 
+"""
+@app.get("/nonblocking")
+async def nonblocking():
+    print("Not")
+    await asyncio.sleep(5)
+    print("Hello")
+    return {"message":"Operation Completed!"}
+
+"""
+Normal Function: 
+"""
+@app.get("/normal")
+def normal():
+    print("Nothing")
+    time.sleep(5)
+    print("Happened")
+    return {"message":"Nothing lol"}
+
 # Run the script: uvicorn main:app --reload
